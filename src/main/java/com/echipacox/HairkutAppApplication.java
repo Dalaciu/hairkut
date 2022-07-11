e94483f282e80838318a848f47a08aaa4ab0cec0
Sebastian Olan <sebastian.nadi@gmail.com>
2022-07-11 17:34:04 +0300

    ok

---
+++ src/main/java/com/echipacox/HairkutAppApplication.java
@@ -0,0 +1,13 @@
+package com.echipacox;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HairkutAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HairkutAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/echipacox/HairkutAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.echipacox;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HairkutAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
